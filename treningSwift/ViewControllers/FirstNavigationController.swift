9ced0e48da24a6a476cbe83b5e2aee0b616a84c0
igor81258 <serry07@rambler.ru>
2022-10-15 00:40:54 +0300

    interaction between controllers

--- treningSwift/ViewControllers/FirstNavigationController.swift
+++ treningSwift/ViewControllers/FirstNavigationController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class FirstNavigationController: UIViewController {
     
+    
+    @IBOutlet var textField: UITextField!
     @IBOutlet var rootButton: UIButton!
     @IBAction func rootButton(_ sender: UIButton) {
         guard let vc = storyboard?.instantiateViewController(identifier: "second") as? SecondNavigationController else{return}
         navigationController?.pushViewController(vc, animated: true)
+        vc.text1 = textField.text!
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destViewController : SecondNavigationController = segue.destination as! SecondNavigationController
+        destViewController.text1 = (textField.text)!
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +30,19 @@ class FirstNavigationController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
-    class SecondNavigationController: UIViewController{
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-        }
+class SecondNavigationController: UIViewController{
+    @IBOutlet var getLabel: UILabel!
+    
+    var text1 = String()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getLabel.text = text1
+        
+        
     }
+}
 
-   
+
 
