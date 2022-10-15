365dbf43299280334b379a33dec14108b375856c
igor81258 <serry07@rambler.ru>
2022-10-15 17:02:31 +0300

    hiddenTabBar

--- treningSwift/ViewControllers/FirstNavigationController.swift
+++ treningSwift/ViewControllers/FirstNavigationController.swift
@@ -26,8 +26,14 @@ class FirstNavigationController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
 class SecondNavigationController: UIViewController{
